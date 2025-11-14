3004c8574729cba5b986ee4f3a0ea34324e7dc41
Sahil Kerkar <skerkar@umich.edu>
2025-11-14 14:54:38 -0500

    Fugutive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,12 +1,17 @@
 return {
-  "lewis6991/gitsigns.nvim",
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        "lewis6991/gitsigns.nvim",
 
-  config = function ()
-    require("gitsigns").setup()
-    vim.keymap.set("n", "<leader>lb", "<CMD>Gitsigns toggle_current_line_blame<CR>", {})
-    vim.keymap.set("n", "<leader>p", "<CMD>Gitsigns preview_hunk<CR>", {})
-    vim.keymap.set("n", "<D-;>", "<CMD>Gitsigns next_hunk<CR>", {})
-    vim.keymap.set("n", "<D-S-;>", "<CMD>Gitsigns prev_hunk<CR>", {})
-    vim.keymap.set("n", "<leader>u", "<CMD>Gitsigns reset_hunk<CR>", {})
-  end
+        config = function ()
+            require("gitsigns").setup()
+            vim.keymap.set("n", "<leader>lb", "<CMD>Gitsigns toggle_current_line_blame<CR>", {})
+            vim.keymap.set("n", "<leader>p", "<CMD>Gitsigns preview_hunk<CR>", {})
+            vim.keymap.set("n", "<D-;>", "<CMD>Gitsigns next_hunk<CR>", {})
+            vim.keymap.set("n", "<D-S-;>", "<CMD>Gitsigns prev_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>u", "<CMD>Gitsigns reset_hunk<CR>", {})
+        end
+    }
 }
